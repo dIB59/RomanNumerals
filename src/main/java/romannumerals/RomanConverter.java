1f1cff318c56c121a1cde91af2e0f5d8cc993583
dIB59 <ibrahim@ebs.net.pk>
2023-05-15 20:26:28 +0200

    almost working

--- src/main/java/romannumerals/RomanConverter.java
+++ src/main/java/romannumerals/RomanConverter.java
@@ -1,4 +1,55 @@
 package romannumerals;
 
 public class RomanConverter {
+
+    enum RomanNumeral {
+        I(1), IV(4), V(5), IX(9), X(10),
+        XL(40), L(50), XC(90), C(100),
+        CD(400), D(500), CM(900), M(1000);
+
+        private int value;
+
+        RomanNumeral(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
+    public static int toArabic(String romanNumbers) {
+
+        int previousNumeral = 0;
+        int i = 0;
+        int totalValue = 0;
+        while (i < romanNumbers.length()) {
+            boolean numberFound = false;
+
+            // Convert Roman Numerals to Arabic
+            for (RomanNumeral numeral : RomanNumeral.values()) {
+                if (romanNumbers.startsWith(numeral.name(), i)) {
+                    totalValue += numeral.getValue();
+
+                    if (previousNumeral =< numeral.getValue()){
+                        totalValue -= numeral.getValue();
+                    }
+
+                    previousNumeral = numeral.getValue();
+
+                    i += numeral.name().length();
+
+                    numberFound = true;
+                    break;
+                }
+            }
+
+            if (!numberFound) {
+                return -1; // Indicate that the input is invalid or not found
+            }
+        }
+
+        return totalValue; // Return the calculated total value
+    }
 }
+
--- src/test/java/romannumerals/RomanNumeralsTest.java
+++ src/test/java/romannumerals/RomanNumeralsTest.java
@@ -1,9 +1,52 @@
 package romannumerals;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.Assert.assertEquals;
 
 public class RomanNumeralsTest {
     @Test
-    void name() {
+    void convertCIToArabic() {
+        assertEquals(101, RomanConverter.toArabic("CI"));
+        }
+
+    @Test
+    void convertXVToArabic() {
+        assertEquals(15, RomanConverter.toArabic("XV"));
     }
-}
+
+    @Test
+    void convertXIToArabic() {
+        assertEquals(11, RomanConverter.toArabic("XI"));
+    }
+
+    @Test
+    void convertXXToArabic() {
+        assertEquals(20, RomanConverter.toArabic("XX"));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "I, 1",
+            "IV, 4",
+            "V, 5",
+            "IX, 9",
+            "X, 10",
+            "XL, 40",
+            "L, 50",
+            "XC, 90",
+            "C, 100",
+            "CD, 400",
+            "D, 500",
+            "CM, 900",
+            "M, 1000",
+
+    })
+    void convertAllRomanToArabic(String Roman, int Arabic) {
+        assertEquals(Arabic, RomanConverter.toArabic(Roman));
+    }
+
+    }
+
